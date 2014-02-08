dfa2af7be8732fa9ddf6b2b6d6b2b3ca9195dd87
Filipe <lipemarques6@gmail.com>
2014-02-07 21:06:15 -0300

    Perfil docente

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var professores;
 
-$( window ).resize(function() {
-  if( $( window ).width() <550 ){
-      $("#menu").panel("close");
-  }else{
-      $("#menu").panel("open");
-  }
+$(window).resize(function () {
+    if ($(window).width() < 550) {
+        $("#menu").panel("close");
+    } else {
+        $("#menu").panel("open");
+    }
 });
 
 $(document).ready(function () {
@@ -32,7 +32,7 @@ function mostrarProfessores() {
     for (var i = 0; i < professores.length; i++) {
         $('#professores_info_listview').append($('<li/>', {}).append($('<a/>', {
             'data-icon': 'star',
-            'href': '#',
+            'href': "javascript:mostrarProfessor('" + professores[i].nome + "')",
             'rel': 'external',
             'text': professores[i].nome
         })));
@@ -43,34 +43,92 @@ function mostrarProfessores() {
     $('#professores_info_listview').listview().listview('refresh');
 }
 
+function mostrarProfessor(nome) {
+
+    var params = {
+        "nome": nome
+    };
+    var str = $.param(params);
+    window.location.href = "docente.html?" + str;
+
+    
+}
+
 function grafico() {
+
+    var qtdAreas = {
+        "Uma": 0,
+        "Duas": 0,
+        "Tres": 0,
+        "Quatro": 0,
+        "Cinco": 0
+    }
+
+    for (var i = 0; i < professores.length; i++) {
+        var qtd = professores[i].areas.length;
+        switch (qtd) {
+        case 1:
+            qtdAreas.Uma = qtdAreas.Uma + 1;
+            break;
+        case 2:
+            qtdAreas.Duas = qtdAreas.Duas + 1;
+            break;
+        case 3:
+            qtdAreas.Tres = qtdAreas.Tres + 1;
+            break;
+        case 4:
+            qtdAreas.Quatro = qtdAreas.Quatro + 1;
+            break;
+        case 5:
+            qtdAreas.Cinco = qtdAreas.Cinco + 1;
+            break;
+        default:
+        case 1:
+            qtdAreas.Uma = qtdAreas.Uma + 1;
+            break;
+        }
+
+
+    }
     var data = [
         {
-            value: 20,
-            color: "#637b85"
+            label: "1",
+            value: qtdAreas.Uma,
+            color: "#F38630",
+            labelColor: 'white',
+            labelFontSize: '16'
     },
         {
-            value: 30,
-            color: "#2c9c69"
+            label: "2",
+            value: qtdAreas.Duas,
+            color: "#E0E4CC",
+            labelColor: 'black',
+            labelFontSize: '16'
     },
         {
-            value: 40,
-            color: "#dbba34"
+            label: "3",
+            value: qtdAreas.Tres,
+            color: "#69D2E7",
+            labelColor: 'white',
+            labelFontSize: '16'
     },
         {
-            value: 10,
-            color: "#c62f29"
+            label: "4+",
+            value: qtdAreas.Quatro + qtdAreas.Cinco,
+            color: "#aa5fb4",
+            labelColor: 'white',
+            labelFontSize: '16'
     }
 
 ];
     var canvas = document.getElementById("hours");
     var ctx = canvas.getContext("2d");
-    new Chart(ctx).Doughnut(data);
-    
+    new Chart(ctx).Pie(data);
+
     canvas = document.getElementById("hours2");
     ctx = canvas.getContext("2d");
     new Chart(ctx).Doughnut(data);
-    
+
     canvas = document.getElementById("hours3");
     ctx = canvas.getContext("2d");
     new Chart(ctx).Doughnut(data);
